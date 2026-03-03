e75d0f1c544d6c82bd4528a44918c8999f0b5f03
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2026-03-03 02:47:29 -0300

    chore: added FloatUtils utilities static class with clamping operations

---
+++ src/Primitives/Utils/FloatUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace GraphicsEngine.Primitives.Utils
+{
+    public static class FloatUtils
+    {
+        public static float Clamp(float value, float min, float max)
+        {
+            return MathF.Min(MathF.Max(value, min), max);
+        }
+
+        public static float Clamp01(float value)
+        {
+            return Clamp(value, 0f, 1f);
+        }
+    }
+}
