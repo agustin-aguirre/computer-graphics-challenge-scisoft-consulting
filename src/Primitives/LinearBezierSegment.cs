022dc41021f6bf81a6f7cc3c434d783ae5af8f04
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2026-03-03 02:42:43 -0300

    chore: added LinearBezierSegment class with curve evaluation

---
+++ src/Primitives/LinearBezierSegment.cs
@@ -0,0 +1,23 @@
+﻿namespace GraphicsEngine.Primitives
+{
+    public class LinearBezierSegment
+    {
+        public Point2D P0 { get; set; }
+        public Point2D P1 { get; set; }
+
+        public LinearBezierSegment(Point2D p0, Point2D p1)
+        {
+            P0 = p0;
+            P1 = p1;
+        }
+
+
+        public virtual Point2D Evaluate(float t)
+        {
+            // Clamping t between 0 and 1
+            t = MathF.Min(MathF.Max(t, 0f), 1f);
+            
+            return P0 + t * (P1 - P0);
+        }
+    }
+}
