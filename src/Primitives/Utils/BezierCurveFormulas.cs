506417b581a28a99bac1783a7e977ed3d3b34fd9
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2026-03-04 11:08:57 -0300

    feat: added overloads that admits only points

--- src/Primitives/Utils/BezierCurveFormulas.cs
+++ src/Primitives/Utils/BezierCurveFormulas.cs
@@ -6,38 +6,68 @@ namespace GraphicsEngine.Primitives.Utils;
 
 public static class BezierCurveFormulas
 {
+    public static Vector2 Linear(Vector2 P0, Vector2 P1, float t)
+    {
+        return P0 + Clamp01(t) * (P1 - P0);
+    }
+
     public static Vector2 Linear(ISegment s, float t)
     {
-        return s.P0 + Clamp01(t) * (s.P1 - s.P0);
+        return Linear(s.P0, s.P1, t);
+    }
+
+
+    public static Vector2 Quadratic(Vector2 P0, Vector2 P1, Vector2 P2, float t)
+    {
+        t = Clamp01(t);
+        float u = 1 - t;
+        return Square(u) * P0 + 2 * u * t * P1 + Square(t) * P2;
     }
 
     public static Vector2 Quadratic(IQuadraticSegment s, float t)
     {
+        return Quadratic(s.P0, s.P1, s.P2, t);
+    }
+
+
+
+    public static Vector2 Cubic(Vector2 P0, Vector2 P1, Vector2 P2, Vector2 P3, float t)
+    {
         t = Clamp01(t);
         float u = 1 - t;
-        return Square(u) * s.P0 + 2 * u * t * s.P1 + Square(t) * s.P2;
+        return Pow(u, 3) * P0 + 3 * Square(u) * t * P1 + 3 * u * Square(t) * P2 + Pow(t, 3) * P3;
     }
 
     public static Vector2 Cubic(ICubicSegment s, float t)
     {
-        t = Clamp01(t);
-        float u = 1 - t;
-        return Pow(u, 3) * s.P0 + 3 * Square(u) * t * s.P1 + 3 * u * Square(t) * s.P2 + Pow(t, 3) * s.P3;
+        return Cubic(s.P0, s.P1, s.P2, s.P3, t);
     }
 
-    public static Vector2 CubicPrime(ICubicSegment s, float t)
+
+    public static Vector2 CubicPrime(Vector2 P0, Vector2 P1, Vector2 P2, Vector2 P3, float t)
     {
         t = Clamp01(t);
         float u = 1 - t;
         // B′(t)=3(1−t)^2(P1​−P0​)+6(1−t)t(P2​−P1​)+3t^2(P3​−P2​)
-        return 3 * Square(u) * (s.P1 - s.P0) + 6 * u * t * (s.P2 - s.P1) + 3 * Square(t) * (s.P3 - s.P2);
+        return 3 * Square(u) * (P1 - P0) + 6 * u * t * (P2 - P1) + 3 * Square(t) * (P3 - P2);
     }
 
-    public static Vector2 CubicSecond(ICubicSegment s, float t)
+    public static Vector2 CubicPrime(ICubicSegment s, float t)
+    {
+        return CubicPrime(s.P0, s.P1, s.P2, s.P3, t);
+    }
+
+
+    public static Vector2 CubicSecond(Vector2 P0, Vector2 P1, Vector2 P2, Vector2 P3, float t)
     {
         t = Clamp01(t);
         float u = 1 - t;
         // B′'(t)=6((1−t)*(P2-2*P1+P0)+t*(P3-2*P2+P1))
-        return 6 * (u * (s.P2 - 2 * s.P1 + s.P0) + t * (s.P3 - 2 * s.P2 + s.P1));
+        return 6 * (u * (P2 - 2 * P1 + P0) + t * (P3 - 2 * P2 + P1));
+    }
+
+    public static Vector2 CubicSecond(ICubicSegment s, float t)
+    {
+        return CubicSecond(s.P0, s.P1, s.P2, s.P3, t);
     }
 }
