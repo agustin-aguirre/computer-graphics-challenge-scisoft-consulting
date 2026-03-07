1e5256b270ee4997894154222dc71afbef981eff
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2026-03-07 01:43:55 -0300

    chore: moved and implemented bounding box struct

---
+++ src/Primitives/BoundingBox.cs
@@ -0,0 +1,33 @@
+﻿using static System.MathF;
+
+namespace GraphicsEngine.Primitives.Interfaces;
+
+/// <summary>
+/// Axis Aligned Bounding Box (AABB)
+/// </summary>
+public struct BoundingBox
+{
+    public readonly Vector2 Min;
+    public readonly Vector2 Center;
+    public readonly Vector2 Max;
+
+    public BoundingBox(IEnumerable<Vector2> points)
+    {
+        float minX = float.MaxValue;
+        float minY = float.MaxValue;
+        float maxX = float.MinValue;
+        float maxY = float.MinValue;
+
+        foreach (var point in points)
+        {
+            minX = Min(point.X, minX);
+            minY = Min(point.Y, minY);
+            maxX = Max(point.X, maxX);
+            maxX = Max(point.Y, maxY);
+        }
+
+        Min = new Vector2(minX, minY);
+        Max = new Vector2(maxX, maxY);
+        Center = (Min + Max) * .5f;
+    }
+}
--- src/Primitives/Interfaces/BoundingBox.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GraphicsEngine.Primitives.Interfaces;
-
-public class BoundingBox
-{
-    public Vector2 Center;
-    public Vector2 Bounds;
-    public Vector2 Extents;
-}
