7c325f4b966a5935addd82b109e6e02c7a8eb826
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2026-03-06 22:48:57 -0300

    chore: Added Calculu static class

---
+++ src/Primitives/Math/Calculus.cs
@@ -0,0 +1,15 @@
+﻿namespace GraphicsEngine.Primitives.Math;
+
+public static class Calculus
+{
+    /// <summary>
+    /// Gauss-Legender Points and Weights for a Polynomial Function of grade 5 or less.
+    /// </summary>
+    public static (float, float)[] GAUSS_LEGENDER_N5_PWs => [
+        (-0.9061798459f, 0.2369268850f),
+        (-0.5384693101f, 0.4786286705f),
+        (0f, 0.5688888889f),
+        (0.5384693101f, 0.4786286705f),
+        (0.9061798459f, 0.2369268850f),
+    ];
+}
