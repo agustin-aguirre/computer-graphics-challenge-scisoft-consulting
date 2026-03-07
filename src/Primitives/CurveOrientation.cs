ae8365df889d1def0c19d7375a3da9a12ed96929
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2026-03-07 01:14:12 -0300

    feat: added doc

--- src/Primitives/CurveOrientation.cs
+++ src/Primitives/CurveOrientation.cs
@@ -1,5 +1,10 @@
 ﻿namespace GraphicsEngine.Primitives;
 
+/// <summary>
+/// CW (-1) = Clockwise,
+/// None (0) = Colineal,
+/// CCW (1) = Counter Clock-Wise
+/// </summary>
 public enum CurveOrientation
 {
     CW = -1,
