4e0af96e5cb6e0ec188fe1ce92ee5eb68b94698e
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2026-03-08 21:04:31 -0300

    feat: renamed to more fitting name

---
+++ src/Primitives/Paths/PointInCurveResult.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphicsEngine.Primitives.Paths;
+
+/// <summary>
+/// Representation of the relative position of a point in regard of a Curve, where Outside = 0, Inside = 1 and Along = 2.
+/// </summary>
+public enum PointInCurveResult
+{
+    Outside = 0,
+    Inside = 1,
+    Along = 2
+}
--- src/Primitives/Paths/PointInPathResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GraphicsEngine.Primitives.Paths;
-
-public enum PointInPathResult
-{
-    Outside = 0,
-    Inside = 1,
-    Along = 2
-}
